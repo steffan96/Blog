d2bbe9d3cd395819e700111c508b86e00f913d23
steffan96 <80476394+steffan96@users.noreply.github.com>
2021-11-07 18:42:14 +0100

    asd

--- db_create.py
+++ db_create.py
@@ -1,5 +1,11 @@
 from app import db
 from run import app
+import os
+import re
 
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
 with app.app_context():
     db.create_all()
