24eb6664874aa5d55a55a8d0eb53d0275e91e7de
steffan96 <80476394+steffan96@users.noreply.github.com>
2021-11-07 20:35:06 +0100

    456

--- db_create.py
+++ db_create.py
@@ -1,7 +1,10 @@
 from app import db
 from run import app
-
+from app.models import Role
 
 
 with app.app_context():
     db.create_all()
+    
+    #create or update user roles
+    Role.insert_roles()
