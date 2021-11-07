93f2c2b769ab091f013cc3e2617d9436227adc3b
steffan96 <80476394+steffan96@users.noreply.github.com>
2021-11-07 18:16:53 +0100

    asd

--- app/config.py
+++ app/config.py
@@ -67,6 +67,15 @@ class ProductionConfig(Config):
         app.logger.addHandler(mail_handler)
 
 class HerokuConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+        # log to stderr
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
     SSL_REDIRECT = True if os.environ.get('DYNO') else False
     
     @classmethod
